97777fa4601f3aba5dbf3ecaa04fec40fc669727
Brooks Patton <brooks@brookzerker.com>
2021-05-25 19:59:19 -0600

    Can cancel out of the Task Details page

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <mwc-button :label="label" @click="handleClick"></mwc-button>
+</template>
+
+<script>
+import "@material/mwc-button";
+
+export default {
+  name: "bb-button",
+  props: {
+    label: String,
+  },
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
--- src/views/TaskDetails.vue
+++ src/views/TaskDetails.vue
@@ -7,6 +7,9 @@
     <div>
       <checkbox label="Completed" :checked="task.completed"></checkbox>
     </div>
+    <div>
+      <bb-button label="Cancel" @click="handleCancel"></bb-button>
+    </div>
   </section>
 </template>
 
@@ -14,12 +17,14 @@
 import BBTitle from "../components/Title";
 import Paragraph from "../components/Paragraph";
 import Checkbox from "../components/Checkbox.vue";
+import Button from "../components/Button";
 
 export default {
   components: {
     "bb-title": BBTitle,
     "bb-paragraph": Paragraph,
     Checkbox,
+    "bb-button": Button,
   },
   data() {
     return {
@@ -29,6 +34,11 @@ export default {
   mounted() {
     const taskId = this.$route.params.id;
     this.task = this.$store.getters.getTaskById(taskId);
+  },
+  methods: {
+    handleCancel() {
+      this.$router.push("/");
+    },
   },
 };
 </script>
