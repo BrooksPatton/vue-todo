0a115db446c8aa892ac4fdc42892cfd248714d9a
Brooks Patton <brooks@brookzerker.com>
2021-05-25 18:49:04 -0600

    can't get tests working in wsl

--- tests/e2e/specs/todo.js
+++ tests/e2e/specs/todo.js
@@ -1,10 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("Todo app", () => {
-    it('As a todo user, I want to see a list of my tasks')
-    it('As a todo user, I want to add a new task')
-    it('As a todo user, I want to delete tasks')
-    it('as a todo user, I want to mark a task as completed')
-    it('As a todo user, I want to see the details of a task')
-    it('As a todo user, I want to edit the details of a task')
+  it("As a Vue Todo user, I want to add a new task", () => {
+    cy.visit("/");
+  });
 });
